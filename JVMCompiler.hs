04761da23581cb09236e8ebe9b275922626cbec3
ksledz <kamila.ktos.sledz@gmail.com>
2020-11-15 20:40:22 +0100

    jvm optimalization: stack depth and order

--- JVMCompiler.hs
+++ JVMCompiler.hs
@@ -44,70 +44,53 @@ printStream = "getstatic java/lang/System/out Ljava/io/PrintStream;\n"
 invokeVirtual :: String
 invokeVirtual = "invokevirtual java/io/PrintStream/println(I)V\n"
 
-stmtsToJVM :: [Stmt] -> JVMonad String
+stmtsToJVM :: [Stmt] -> JVMonad (String, Integer)
 
 stmtsToJVM (SAss string aexpr:rest) = do
-  code <- aexprToJVM aexpr
+  (code, depth) <- aexprToJVM aexpr
   map <- get
   when (not (Map.member string map))$ modify (Map.insert string (1 + (toInteger $ Map.size map)))
   map <- get
   let id = Map.findWithDefault 0 string map
-  jvmRest <- stmtsToJVM rest
-  return $ code ++ storeVar id ++"\n" ++ jvmRest
+  (jvmRest, depth2) <- stmtsToJVM rest
+  return (code ++ storeVar id ++"\n" ++ jvmRest, max depth depth2)
 
 stmtsToJVM (SExp aexpr:rest) = do
-  code <- aexprToJVM aexpr
-  jvmRest <-  stmtsToJVM rest
-  return $ printStream ++ code ++ invokeVirtual ++ jvmRest
+  (code, depth) <- aexprToJVM aexpr
+  (jvmRest, depth2) <-  stmtsToJVM rest
+  return (code ++ printStream ++ "swap\n" ++ invokeVirtual ++ jvmRest, max (max depth 2) depth2)
 
 stmtsToJVM ([]) =
-  return ""
+  return ("", 0)
 
-aexprToJVM :: Exp -> JVMonad (String)
+aexprToJVM :: Exp -> JVMonad (String, Integer)
 
 aexprToJVM (ExpVar string) =  do
   var <- get
   -- UWAGA sprawdzanie niezdefiniowane zmiennej also w llvm 
   let i = Map.findWithDefault 0 string var
-  return $ pushVar i ++ "\n"
+  return  (pushVar i ++ "\n", 1)
 
-aexprToJVM (ExpLit integer) = return (pushConst integer ++ "\n")
+aexprToJVM (ExpLit integer) = return (pushConst integer ++ "\n", 1)
 
-aexprToJVM (ExpAdd ex1 ex2) = opToJVM ex1 ex2 "add"
-aexprToJVM (ExpMul ex1 ex2) = opToJVM ex1 ex2 "mul"
-aexprToJVM (ExpSub ex1 ex2) = opToJVM ex1 ex2 "sub"
-aexprToJVM (ExpDiv ex1 ex2) = opToJVM ex1 ex2 "div"
+aexprToJVM (ExpAdd ex1 ex2) = opToJVM ex1 ex2 "add" True
+aexprToJVM (ExpMul ex1 ex2) = opToJVM ex1 ex2 "mul" True
 
-opToJVM :: Exp -> Exp -> String -> JVMonad (String)
-opToJVM ex1 ex2 op = do
-  code1 <- aexprToJVM ex1
-  code2 <- aexprToJVM ex2
-  return $ code1 ++ code2 ++ " i" ++ op ++"\n"
+aexprToJVM (ExpSub ex1 ex2) = opToJVM ex1 ex2 "sub" False
+aexprToJVM (ExpDiv ex1 ex2) = opToJVM ex1 ex2 "div" False
 
-getOpLimit :: Exp -> Exp -> Integer
-getOpLimit e1 e2 =
-  let e1L = getExpLimit e1 in let e2L = getExpLimit e2 in max e1L (1 + e2L)
--- źle 
-getExpLimit :: Exp -> Integer
-getExpLimit (ExpVar _) = 1
-getExpLimit (ExpLit _) = 1
-getExpLimit (ExpAdd e1 e2) = getOpLimit e1 e2
-getExpLimit (ExpMul e1 e2) = getOpLimit e1 e2
-getExpLimit (ExpSub e1 e2) = getOpLimit e1 e2
-getExpLimit (ExpDiv e1 e2) = getOpLimit e1 e2
+opToJVM :: Exp -> Exp -> String -> Bool -> JVMonad (String, Integer)
+opToJVM ex1 ex2 op comm  = do
+  (code1, depth1) <- aexprToJVM ex1
+  (code2, depth2) <- aexprToJVM ex2
+  if depth2 > depth1 then return (code2 ++ code1 ++ (if not comm then "swap\n" else "") ++ " i" ++ op ++ "\n", depth2)
+  else return (code1 ++ code2 ++ " i" ++ op ++"\n", max depth1 (depth2 + 1))
 
-getStmtLimit :: Stmt -> Integer
-getStmtLimit (SAss _ exp) = getExpLimit exp
-getStmtLimit (SExp exp) = 1 + getExpLimit exp
-
-getLimit :: [Stmt] -> Integer
-getLimit = foldr (max . getStmtLimit) 1
 
 allToJVM :: Program -> String -> String
 allToJVM (Prog stmts) fname  =
-  let (code, map) = runState(stmtsToJVM stmts) Map.empty
-  in let lStack = getLimit stmts
-  in let lLocals = 1 + Map.size map -- x -- xDD
+  let ((code, depth), map) = runState(stmtsToJVM stmts) Map.empty
+  in let lLocals = 1 + Map.size map
 
-  in pre fname ++ ".limit stack " ++ show lStack ++"\n.limit locals " ++
+  in pre fname ++ ".limit stack " ++ show depth ++"\n.limit locals " ++
     show lLocals ++"\n"++ code ++ post
